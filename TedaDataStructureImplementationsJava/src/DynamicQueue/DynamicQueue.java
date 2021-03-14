a1f804af7bd90d5b0e66ae58e77513cab8c5acb0
teddyashe17 <teddyashe107@gmail.com>
2021-03-14 15:54:58 +0300

    queue implementation using java ArrayList

---
+++ TedaDataStructureImplementationsJava/src/DynamicQueue/DynamicQueue.java
@@ -0,0 +1,62 @@
+package dynamic_queue;
+
+import java.util.ArrayList;
+
+public class DynamicQueue {
+	ArrayList<Integer> list;
+	int head = 0;
+
+	public DynamicQueue() {
+		list = new ArrayList<Integer>();
+	}
+
+	public void enqueue(int value) {
+		list.add(value);
+	}
+
+	public int dequeue() {
+		if (!isEmpty()) {
+			int popValue = list.get(head);
+			list.remove(head);
+			return popValue;
+		} else {
+			System.out.println("The stack is already empty");
+			return -1;
+		}
+
+	}
+
+	public boolean isEmpty() {
+		if (list.isEmpty()) return true;
+		else
+			return false;
+
+	}
+
+	public int peek() {
+		return list.get(head);
+	}
+
+	public void print() {
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+
+	public static void main(String[] args) {
+		DynamicQueue queue = new DynamicQueue();
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.enqueue(4);
+		queue.enqueue(5);
+		queue.enqueue(6);
+
+		queue.print();
+		System.out.println("After Dequeue instruction");
+		queue.dequeue();
+		queue.print();
+
+	}
+
+}

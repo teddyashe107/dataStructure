91e1b42f81dcc3a7151a34cbe8777607cd737710
teddyashe17 <teddyashe107@gmail.com>
2021-03-14 15:26:28 +0300

    stack implementation using both languages

--- TedaDataStructureImplementations/src/dynamicStack/DynamicStack.java
+++
@@ -1,8 +0,0 @@
-package dynamicStack;
-
-public class DynamicStack {
-	public static void main(String[] args) {
-		System.out.println("Hello Teddys");
-	}
-
-}
---
+++ TedaDataStructureImplementationsJava/src/dynamicStack/DynamicStack.java
@@ -0,0 +1,8 @@
+package dynamicStack;
+
+public class DynamicStack {
+	public static void main(String[] args) {
+		System.out.println("Hello Teddys");
+	}
+
+}
